258750653117fe3a374d6b83fdc58cb06b52f4e2
mohamedisakr <mohamedisakr@gmail.com>
2023-06-30 15:18:15 +0300

    unit test slide window

--- sliding_window/contiguous_subarrays_size.py
+++ sliding_window/contiguous_subarrays_size.py
@@ -14,7 +14,43 @@ def find_avg_sub_array(arr, k):
     return result
 
 
-def find_avg_sub_array_slide(arr, k):
+def find_avg_sub_array_slide(arr: list[float], k: int):
+    """Find the average of all contiguous subarrays of size k in the given array.
+
+    This function uses a sliding window technique to iterate over the array and
+    calculate the average of each subarray of size k. It returns a list of
+    averages.
+
+    Args:
+        arr: The input array of numbers. Must not be empty.
+        k: The size of the subarray. Must be positive and not larger than the length of the array.
+
+    Returns:
+        A list of averages of all contiguous subarrays of size k in the input array.
+
+    Raises:
+        ValueError: If k is zero or negative
+        ValueError: If k is larger than the length of the array.
+        TypeError: If k is not an integer.
+    """
+    # Check for preconditions
+
+    # check if k is an iteger
+    if not isinstance(k, int):
+        raise TypeError("k must be an integer")
+
+    # assert arr, "arr must not be empty"
+    if len(arr) <= 0:
+        raise ValueError("arr must not be empty")
+
+    # assert k > 0, "k must be positive"
+    if k <= 0:
+        raise ValueError("k must be positive")
+
+    # assert k <= len(arr), "k must not be larger than the length of the array"
+    if k > len(arr):
+        raise ValueError("k must not be larger than the length of the array")
+
     result = []
     total = 0.0
     start = 0
@@ -25,6 +61,14 @@ def find_avg_sub_array_slide(arr, k):
             result.append(total/k)
             total -= arr[start]
             start += 1
+
+    # Check for postconditions
+    assert len(result) == len(arr) - k + 1, \
+        "result must have the same length as the number of subarrays"
+
+    assert all(isinstance(x, float)
+               for x in result), "result must contain only floats"
+
     return result
 
 
--- tests/find_avg_sub_array_slide_test.py
+++ tests/find_avg_sub_array_slide_test.py
@@ -1,24 +1,53 @@
+"""
+test find_avg_sub_array_slide function
+"""
+
 from unittest import TestCase, main
 from sliding_window.contiguous_subarrays_size import find_avg_sub_array_slide
 
 
-class TestFindAvgSubArray(TestCase):
+class TestFindAvgSubArraySlide(TestCase):
 
     def test_empty_array(self):
         # Test case for an empty array
         arr = []
         k = 5
-        expected = []
+
+        # with self.assertRaises(ValueError) as context:
+        #     print(f'test_empty_array --->> {str(context.exception)}')
+        #     find_avg_sub_array_slide(arr, k)
+        # self.assertEqual(str(context.exception), "arr must not be empty")
+        with self.assertRaises(ValueError):
+            find_avg_sub_array_slide(arr, k)
+
+    def test_normal_case(self):
+        """Test a normal case with valid inputs and outputs."""
+        arr = [1, 3, 2, 6, -1, 4, 1, 8, 2]
+        k = 5
+        expected = [2.2, 2.8, 2.4, 3.6, 2.8]
         actual = find_avg_sub_array_slide(arr, k)
         self.assertEqual(expected, actual)
 
-    def test_k_larger_than_array_length(self):
-        # Test case for k larger than the array length
+    def test_zero_k(self):
+        """Test a case with zero as k."""
         arr = [1, 2, 3]
-        k = 5
-        expected = []
-        actual = find_avg_sub_array_slide(arr, k)
-        self.assertEqual(expected, actual)
+        k = 0
+        # with self.assertRaises(ValueError) as context:
+        #     find_avg_sub_array_slide(arr, k)
+        # self.assertEqual(str(context.exception), "k must be positive")
+        with self.assertRaises(ValueError):
+            find_avg_sub_array_slide(arr, k)
+
+    def test_k_larger_than_length(self):
+        """Test a case with k larger than the length of the array."""
+        arr = [1, 2, 3]
+        k = 4
+        # with self.assertRaises(ValueError) as context:
+        #     find_avg_sub_array_slide(arr, k)
+        # self.assertEqual(str(context.exception),
+        #                  "k must not be larger than the length of the array")
+        with self.assertRaises(ValueError):
+            find_avg_sub_array_slide(arr, k)
 
     def test_k_equal_to_array_length(self):
         # Test case for k equal to the array length
@@ -36,6 +65,57 @@ class TestFindAvgSubArray(TestCase):
         actual = find_avg_sub_array_slide(arr, k)
         self.assertEqual(expected, actual)
 
+    def test_negative_k(self):
+        """Test a case with a negative k."""
+        arr = [1, 2, 3]
+        k = -1
+        # with self.assertRaises(ValueError) as context:
+        #     find_avg_sub_array_slide(arr, k)
+        # self.assertEqual(str(context.exception), "k must be positive")
+        with self.assertRaises(ValueError):
+            find_avg_sub_array_slide(arr, k)
 
+# ---------------------
+
+    def test_single_element_array(self):
+        """ A test method that checks if the function returns the correct 
+        average for a single element array and k = 1"""
+        arr = [5]
+        k = 1
+        actual = find_avg_sub_array_slide(arr, k)
+        expected = [5.0]
+        self.assertEqual(actual, expected)
+
+    def test_array_equal_to_k(self):
+        """ A test method that checks if the function returns the correct 
+        averages for an array of length k"""
+        arr = [1, 2, 3, 4, 5]
+        k = 5
+        actual = find_avg_sub_array_slide(arr, k)
+        expected = [3.0]
+        self.assertEqual(actual, expected)
+
+    def test_array_with_negative_numbers(self):
+        """ A test method that checks if the function returns the correct 
+        averages for an array with negative numbers"""
+        arr = [-1, -2, -3, -4, -5]
+        k = 3
+        actual = find_avg_sub_array_slide(arr, k)
+        expected = [-2.0, -3.0, -4.0]
+
+        self.assertEqual(actual, expected)
+
+    def test_array_with_floats(self):
+        """A test method that checks if the function returns the correct 
+        averages for an array with floating point numbers"""
+        arr = [1.5, 2.5, 3.5, 4.5, 5.5]
+        k = 4
+        actual = find_avg_sub_array_slide(arr, k)
+        expected = [3.0, 4.0]
+
+        self.assertEqual(actual, expected)
+
+
+# ------------------
 if __name__ == '__main__':
     main()
