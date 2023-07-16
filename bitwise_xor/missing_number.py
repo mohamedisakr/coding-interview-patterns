ddde16a929cda64162518c845c8b1fb211214d91
mohamedisakr <mohamedisakr@gmail.com>
2023-07-16 07:44:50 +0300

    missing number sort & set

---
+++ bitwise_xor/missing_number.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/missing-number/solutions/2081388/4-approaches-sorting-hash-xor-math/
+def find_missing_number_sort(nums: list[int]):
+    n = len(nums)
+    nums.sort()
+
+    for i in range(n):
+        if nums[i] != i+1:
+            return i+1
+
+    return n
+
+
+def find_missing_number_hashset(nums: list[int]):
+    n = len(nums)
+    hashset = set()
+
+    for i in range(n):
+        hashset.add(nums[i])
+
+    for i in range(1, n+1):
+        if i not in hashset:
+            return i
+
+    return n
+
+
+# arr = [1, 5, 2, 6, 4]
+# actual = find_missing_number_sort(arr)
+# expected = 3
+
+# print(f'missing number : {actual}')
+# print(f'actual : {actual} = expected : {expected}')
+
+arr1 = [1, 5, 2, 6, 4]
+actual1 = find_missing_number_hashset(arr1)
+expected1 = 3
+
+print(f'missing number : {actual1}')
+print(f'actual : {actual1} = expected : {expected1}')
